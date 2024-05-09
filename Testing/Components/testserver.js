0d9c591ffb1820458b24fac1517f96bf0f72e339
mcyellowbird <idrieseaglemasuak@gmail.com>
2024-05-10 00:57:43 +1000

    Updates to style guide & added charts

---
+++ Testing/Components/testserver.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors'); // Import the cors middleware
+
+// Use the cors middleware
+app.use(cors());
+
+// Route to serve the JSON data
+app.get('/data', (req, res) => {
+    let jsonData = {
+        CSIT110: [{ date: 'Mon', attendance: 85 }, { date: 'Tue', attendance: 90 }, { date: 'Wed', attendance: 88 }, { date: 'Thu', attendance: 82 }, { date: 'Fri', attendance: 86 }],
+        CSIT111: [{ date: 'Mon', attendance: 78 }, { date: 'Tue', attendance: 82 }, { date: 'Wed', attendance: 80 }, { date: 'Thu', attendance: 75 }, { date: 'Fri', attendance: 80 }],
+        CSIT113: [{ date: 'Mon', attendance: 90 }, { date: 'Tue', attendance: 92 }, { date: 'Wed', attendance: 88 }, { date: 'Thu', attendance: 85 }, { date: 'Fri', attendance: 87 }],
+        CSIT114: [{ date: 'Mon', attendance: 82 }, { date: 'Tue', attendance: 85 }, { date: 'Wed', attendance: 80 }, { date: 'Thu', attendance: 78 }, { date: 'Fri', attendance: 82 }]
+    };
+
+    res.json(jsonData);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
