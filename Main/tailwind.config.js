ad3a4a8c8780f9e15ef9a8ce356a7df72a126953
mcyellowbird <idrieseaglemasuak@gmail.com>
2024-07-09 13:41:41 +1000

    tailwind fix

--- Main/tailwind.config.js
+++ Main/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
 module.exports = {
-  content: ['./src/**/*.{html,js,php}', './*.php'],
+  content: ['./src/**/*.{html,js,php,css}', './*.{php,html}'],
   darkMode: 'class',
   theme: {
     extend: {
